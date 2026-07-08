a478ebaee5d35deb3f943af777084eddb53a686d
IsPekToP <inspektop1@gmail.com>
2026-07-08 20:35:12 +0200

    добавление menu.js

---
+++ src/scripts/menu.js
@@ -0,0 +1,8 @@
+// MENU
+
+const menu = document.querySelector('.menu');
+
+menu?.addEventListener('click', () =>{
+    const isExpanded = menu.getAttribute('aria-expanded') === 'true';
+    menu.setAttribute('aria-expanded', `${!isExpanded}`);
+});
