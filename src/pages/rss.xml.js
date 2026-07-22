359d64e17ead8f889ca51b48e1502dc4d28421eb
IsPekToP <inspektop1@gmail.com>
2026-07-22 19:40:09 +0200

    add rss.xml.js

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,12 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+
+export async function GET(context) {
+    return rss({
+        title: 'Astro Leaner / Blog',
+        description: 'My journey learning Astro',
+        site: context.site,
+        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        customData: `<language>ru-RU</language>`,
+    })
+}
